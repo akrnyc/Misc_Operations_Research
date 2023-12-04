a27d31326b712918be58490022ae02901d3bb90a
akrnyc <alxramnarine@gmail.com>
2023-12-04 18:19:32 -0500

    Update healthcare_weightloss.py

--- healthcare_weightloss.py
+++ healthcare_weightloss.py
@@ -27,12 +27,13 @@ customers = pd.DataFrame({'customer_id': random.sample(range(1000, 3000), n_cust
 #generate medical history df
 c_ids = customers.customer_id.to_list()
 medical_history = pd.DataFrame({'customer_id': c_ids,
+                                'age': [random.randint(40, 80) for x in range(len(c_ids))],
                                 'height_inches': [round(random.normalvariate(67.0, 2.5), 1) for x in range(len(c_ids))],
                                 'initial_weight': [round(random.normalvariate(190, 35), 2) for x in range(len(c_ids))],
                                 'current_weight': [round(random.normalvariate(190, 50), 2) for x in range(len(c_ids))],
                                 'diabetic_risk': [bool(random.getrandbits(1)) for x in range(len(c_ids))],
                                 'cardiac_risk': [bool(random.getrandbits(1)) for x in range(len(c_ids))],
-                                'has_insurance': [bool(random.getrandbits(1)) for x in range(len(c_ids))]
+                                'insurance_coverage': random.choices(['None', 'Full', 'Partial'], weights=(0.30, 0.50, 0.20), k=len(c_ids))
                              })
 
 #generate products df
@@ -47,7 +48,7 @@ products = pd.DataFrame({'product_id': [fake.passport_number() for x in range(2)
 #generate marketing df
 combos = []
 p_ids = products['product_id'].to_list()
-campaigns = ['influencer', 'wellness_check']
+campaigns = ['insurance', 'wellness_check']
 utm_sources = ['facebook', 'amazon', 'instagram', 'billboard', 'subway', 'referral']
 
 for p in p_ids:
@@ -80,7 +81,7 @@ marketing['impressions'] = [int(round(random.normalvariate(100000, 70), 0)) for 
 marketing = marketing.reset_index(drop=True)
 
 #generate sales df
-n_sales = 120000
+n_sales = 12000
 statuses = ['Delivered', 'Canceled', 'Returned']
 couriers = ['Amazon', 'FedEx', 'UPS']
 
@@ -91,25 +92,38 @@ sales_scales = pd.DataFrame({'order_id': [fake.isbn13() for x in range(len(c_ids
                       'service_level': random.choices(['Standard', 'Expedited'], weights=(0.30, 0.70), k=len(c_ids)),
                       'product_id': [p_id for x in range(len(c_ids))],
                       'quantity': [1 for x in range(len(c_ids))],
-                      'customer_id': [random.choice(c_ids) for x in range(len(c_ids))]
+                      'customer_id': c_ids
                       })
 sales_scales['order_date'] = [(customers[customers['customer_id'] == x]['join_date'].values[0] + timedelta(days=random.randint(1, 5))) for x in sales_scales['customer_id'].to_list()]
 sales_scales['ship_date'] = [x + timedelta(days=random.randint(1, 9)) for x in sales_scales['order_date'].to_list()]
 
+c_ids = sales_scales[sales_scales['status'] == 'Delivered']['customer_id'].to_list()
 p_id = products[products['category'] == 'meal_kit']['product_id'].values[0]
-sales_mealkits = pd.DataFrame({'order_id': [fake.isbn13() for x in range(n_sales)],
-                      'status': random.choices(statuses, weights=(0.800, 0.150, 0.050), k=n_sales),
-                      'courier': random.choices(couriers, weights=(0.50, 0.20, 0.30), k=n_sales),
-                      'service_level': random.choices(['Standard', 'Expedited'], weights=(0.30, 0.70), k=n_sales),
-                      'product_id': [p_id for x in range(n_sales)],
-                      'quantity': [random.randint(1, 3) for x in range(n_sales)],
-                      'customer_id': [random.choice(c_ids) for x in range(n_sales)]
+sales_mealkits = pd.DataFrame({'order_id': [fake.isbn13() for x in range(len(c_ids))],
+                      'status': random.choices(statuses, weights=(0.950, 0.050, 0.000), k=len(c_ids)),
+                      'courier': random.choices(couriers, weights=(0.50, 0.20, 0.30), k=len(c_ids)),
+                      'product_id': [p_id for x in range(len(c_ids))],
+                      'quantity': [random.randint(1, 3) for x in range(len(c_ids))],
+                      'customer_id': c_ids
                       })
 sales_mealkits['order_date'] = [(customers[customers['customer_id'] == x]['join_date'].values[0] + timedelta(days=random.randint(5, 10))) for x in sales_mealkits['customer_id'].to_list()]
 sales_mealkits['ship_date'] = [x + timedelta(days=random.randint(1, 5)) for x in sales_mealkits['order_date'].to_list()]
 
+#recurring orders
+while sales_mealkits.shape[0] < n_sales:
+    sales_more_mealkits = pd.DataFrame({'order_id': [fake.isbn13() for x in range(len(c_ids))],
+                        'status': random.choices(statuses, weights=(0.950, 0.050, 0.000), k=len(c_ids)),
+                        'courier': random.choices(couriers, weights=(0.50, 0.20, 0.30), k=len(c_ids)),
+                        'product_id': [p_id for x in range(len(c_ids))],
+                        'quantity': [random.randint(1, 2) for x in range(len(c_ids))],
+                        'customer_id': c_ids
+                        })
+    sales_more_mealkits['order_date'] = [(sales_mealkits[sales_mealkits['customer_id'] == x]['ship_date'].max() + timedelta(days=random.randint(3, 12))) for x in sales_more_mealkits['customer_id'].to_list()]
+    sales_more_mealkits['ship_date'] = [x + timedelta(days=random.randint(1, 7)) for x in sales_more_mealkits['order_date'].to_list()]
+    sales_mealkits = pd.concat([sales_mealkits, sales_more_mealkits], axis=0, ignore_index=True)
+
+
 sales = pd.concat([sales_scales, sales_mealkits], axis=0, ignore_index=True)
-sales = sales.sample(frac=1)
 
 with pd.ExcelWriter('/Users/alex/Downloads/weightlossservice.xlsx', engine='openpyxl') as writer:
     customers.to_excel(writer, sheet_name='customers')
